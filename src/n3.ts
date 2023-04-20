d57e0d6f36b86356851821b68bbfdc1de8f44000
soshroom <soshroom@novelty.su>
2023-04-20 21:24:01 +0400

    fix n3

--- src/n3.ts
+++ src/n3.ts
@@ -1,19 +1,23 @@
 function fn(words: string[]) {
     const out: string[] = [];
-    const chars: { [key: string]: string[] } = {};
-    for (const word of words) {
-        for (const char of word) {
-            if (chars[char]) chars[char].push(char);
-            else chars[char] = [char];
-
-            if (chars[char].length == words.length) {
-                out.push(char);
-                chars[char] = [];
+    const tmpWords: string[] = [...words]
+    for (let i = 0; i < tmpWords[0].length; i += 1){
+        let count: number = 1;
+        for (let j = 1; j < tmpWords.length; j += 1){
+            const tempReg = new RegExp(tmpWords[0][i]);
+            const findLetter = tmpWords[j].match(tempReg);
+            if (findLetter){
+                const newWord: string[] = tmpWords[j].split("");
+                if(findLetter.index)
+                newWord.splice(findLetter.index, 1);
+                tmpWords[j] = newWord.join("");
+                count += 1;
             }
         }
+        if (count === words.length) out.push(tmpWords[0][i]);
     }
     return out;
 }
 
-console.log(fn(["bella", "label", "roller"]), ["e", "l", "l"]);
+console.log(fn(["bellal", "label", "roller"]), ["e", "l", "l"]);
 console.log(fn(["cool", "lock", "cook"]), ["c", "o"]);
